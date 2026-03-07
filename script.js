fab7d30dbab5256ddc6caac97cce35120a487abf
Joy Goswami <joygoswaminiloy2023@gmail.com>
2026-03-07 12:47:39 +0600

    badge priority fixed

--- script.js
+++ script.js
@@ -265,7 +265,8 @@ document.getElementById('modal_info').innerHTML = `
   <div>
     <h4 class="text-gray-600 text-sm mb-1">Priority:</h4>
     <span class="inline-block px-4 py-1 rounded-full text-sm font-semibold uppercase tracking-wider ${
-     
+      issue.priority === 'high' ? 'bg-red-100 text-red-600' :
+      issue.priority === 'medium' ? 'bg-yellow-100 text-yellow-400' :
       'bg-gray-300 text-gray-800'
     }">
       ${issue.priority}
